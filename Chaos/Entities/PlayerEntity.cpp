79daa52ba68f5e3368d322c90ff0b1b1c2d9d475
Miles Smith <milesdavidsmith@gmail.com>
2017-01-04 02:00:55 -0800

    blinky boxes

--- Chaos/Entities/PlayerEntity.cpp
+++ Chaos/Entities/PlayerEntity.cpp
@@ -30,7 +30,12 @@ void PlayerEntity::render(Graphics *g)
         pos((int)getXPosition() - boxSize, (int)getYPosition() - boxSize),
         size(boxSize, boxSize);
 
-    g->setColor((int)(red_*pow(sin(3 * velocity_ + time * 1), 2)), (int)(green_*pow(sin(velocity_ + time * 2), 2)), (int)(blue_*pow(2*velocity_ + sin(time * 3), 2)), 150);
+    g->setColor(
+        (int)(red_*pow(sin(3 * velocity_ + time * 1), 2)),
+        (int)(green_*pow(sin(velocity_ + time * 2), 2)),
+        (int)(blue_*pow(2*velocity_ + sin(time * 3), 2)),
+        150
+    );
     g->fillRect(pos, size);
     g->setColor(0, 0, 0, 255);
     g->drawRect(pos, size);
